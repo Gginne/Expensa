caa3f17738d672e8157713d1c934e68fad0a4604
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-07-24 16:16:57 -0400

    Corrected auth routing

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require("../controllers/auth.controller")
 
-router.route("/auth")
-      .get(AuthController.loginreg)
       
 router.route("/login")
       .post( AuthController.login)
