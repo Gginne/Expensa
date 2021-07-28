5d1f9ae80c9ec94089ed493647d48d065cd65eeb
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-07-28 14:14:01 -0400

    Changed from tables to forms

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -52,43 +52,42 @@ class Entry extends Component {
         const isFilled = Object.keys(data).some(key => Boolean(data[key]))
         const {type, amount, category, description, datetime} = data
         return (
-            <tr>
-                <td>
+            <form className="row p-2">
+                <div className="col-xs-6 col-sm-4 col-md-2 mt-1">
                     <select className="form-control" name="type" aria-label="Default select example" onChange={e => this.handleChange(e)} disabled={!edit}>
                         <option value="" selected={type === ""}  >Type</option>
                         <option value="expenses" selected={type === 'expenses'}>Expense</option>
                     </select>
-                </td>
-                <td>
+                </div>
+                <div className="col-xs-6 col-sm-4 col-md-2 mt-1">
                     <select className="form-control" name="category" aria-label="Default select example" onChange={e => this.handleChange(e)} disabled={!edit}>
                         <option value="" selected={category === ""} >Category</option>
                         <option value="food" selected={category === "food"} >Food</option>
                     </select>
-                </td>
-                <td >
+                </div>
+                <div className="col-xs-6 col-sm-4 col-md-1 mt-1">
                     <input type="number" className="form-control" onChange={e => this.handleChange(e)} value={amount} min={1}  name="amount" placeholder="Amount" disabled={!edit}/>
-                </td>
-                <td>
+                </div>
+                <div className="col-xs-6 col-sm-5 col-md-2 mt-1">
                     <input type="text" className="form-control" onChange={e => this.handleChange(e)} value={description} name="description" placeholder="Description" disabled={!edit}/>
-                </td>
-                <td>
+                </div>
+                <div className="col-xs-6 col-sm-7 col-md-3 mt-1">
                     <input type="datetime-local" className="form-control" onChange={e => this.handleChange(e)} value={datetime} name="datetime" placeholder="Date/Time" disabled={!edit}/>
-                </td>
-                <td col="2">
-                    {edit ? (
-                        <div class="btn-group" role="group" aria-label="Entry Form Controls">
-                         <button class={`btn btn-${id == null ? 'success' : 'info'}`} onClick={() => this.saveEntry()} disabled={isIncomplete} >{id == null ? 'Add' : 'Save'}</button>
-                         <button class={`btn btn-danger`} onClick={() => this.clearFields()} disabled={!isFilled} >Clear</button>
-                        </div>
-                    ) : (
-                        <div class="btn-group" role="group" aria-label="Entry Controls">
-                            <button type="button" class="btn btn-warning" onClick={() => this.setEditState()}>Edit</button>
-                            <button type="button" class="btn btn-danger" onClick={() => this.deleteEntry()}>Delete</button>
-                        </div>
-                    )}
+                </div>
+                {edit ? (
+                    <div class="btn-group col-xs-6 col-sm-12 col-md-1 mt-2" role="group" aria-label="Entry Form Controls">
+                        <button class={`btn btn-${id == null ? 'success' : 'info'}`} onClick={() => this.saveEntry()} disabled={isIncomplete} >{id == null ? 'Add' : 'Save'}</button>
+                        <button class={`btn btn-danger`} onClick={() => this.clearFields()} disabled={!isFilled} >Clear</button>
+                    </div>
+                ) : (
+                    <div class="btn-group col-xs-6 col-sm-12 col-md-1 mt-2" role="group" aria-label="Entry Edit Controls">
+                        <button class="btn btn-warning" onClick={() => this.setEditState()}>Edit</button>
+                        <button class="btn btn-danger" onClick={() => this.deleteEntry()}>Delete</button>
+                    </div>
+                )}
                    
-                </td>
-            </tr>
+                
+            </form>
         )
     }
 }
