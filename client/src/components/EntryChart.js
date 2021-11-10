73e474588a62f3bc3178f15d71c88eb0854d6bc4
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-11-09 23:53:20 -0500

    Added category name color hashing

--- client/src/components/EntryChart.js
+++ client/src/components/EntryChart.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react'
+import toMaterialStyle from 'material-color-hash';
 import { Doughnut } from 'react-chartjs-2';
 
 class EntryChart extends Component {
-    toColor = str =>{
-        var hash = 5381;
-        for (var i = 0; i < str.length; i++) {
-          hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
-        }
-        var r = (hash & 0xFF0000) >> 16;
-        var g = (hash & 0x00FF00) >> 8;
-        var b = hash & 0x0000FF;
-        return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2); 
-    }
     
     renderCategories = () => {
         let categories = {}
@@ -32,7 +23,7 @@ class EntryChart extends Component {
                 {
                     label: 'Entry Chart',
                     data: Object.values(categories),
-                    backgroundColor: Object.keys(categories).map(c => this.toColor(c))
+                    backgroundColor: Object.keys(categories).map(c => toMaterialStyle(c).backgroundColor)
                 },
             ],
         })
