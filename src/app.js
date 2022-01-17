b386f41d8468f1d6bb576d9e07dcb580399521e7
Gginne <23042760+Gginne@users.nonreply.github.com>
2022-01-17 15:11:44 -0500

    Fixed path variable

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //Imports
 require("dotenv").config()
 const express = require("express")
+const path = require("path")
 const cors = require("cors")
 const cookieParser = require('cookie-parser')
 
@@ -11,6 +12,7 @@ app.set('port', process.env.PORT || 5000)
 
 //Middleware
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '/../client/build')))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
